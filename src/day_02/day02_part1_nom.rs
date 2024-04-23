fc9fa80d2dd8df02d6cbbd8454b6a606e9c9bf3b
Andy Griffiths <griffiths_andy@icloud.com>
2024-04-23 21:44:21 +0100

    removed duped test

--- src/day_02/day02_part1_nom.rs
+++ src/day_02/day02_part1_nom.rs
@@ -83,16 +83,6 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_process() {
-        let input = "Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-        assert_eq!(8, process(input));
-    }
-
-    #[test]
     fn test_part_one_test() {
         assert_eq!(8, process(include_str!("day02_test.txt")));
     }
