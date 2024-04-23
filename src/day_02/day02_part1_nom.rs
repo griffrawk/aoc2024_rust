db191e233872f1e4ef3525a139c20802c6c13ca8
Andy Griffiths <griffiths_andy@icloud.com>
2024-04-23 17:28:39 +0100

    change assert to u32

--- src/day_02/day02_part1_nom.rs
+++ src/day_02/day02_part1_nom.rs
@@ -79,7 +79,7 @@ fn parse_games(input: &str) -> IResult<&str, Vec<Game>> {
     Ok((input, games))
 }
 
-pub fn process(input: &str) -> miette::Result<String, AocError> {
+pub fn process(input: &str) -> miette::Result<u32, AocError> {
     let map = BTreeMap::from([("red", 12), ("green", 13), ("blue", 14)]);
     let games = parse_games(input).expect("should parse");
 
@@ -88,7 +88,7 @@ pub fn process(input: &str) -> miette::Result<String, AocError> {
         .iter()
         .filter_map(|game| game.valid_for_cube_set(&map))
         .sum::<u32>()
-        .to_string())
+    )
 }
 
 #[cfg(test)]
@@ -102,19 +102,19 @@ Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
 Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
 Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
 Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green";
-        assert_eq!("8", process(input)?);
+        assert_eq!(8, process(input)?);
         Ok(())
     }
 
     #[test]
     fn test_part_one_test() -> miette::Result<()> {
-        assert_eq!("8", process(include_str!("day02_test.txt"))?);
+        assert_eq!(8, process(include_str!("day02_test.txt"))?);
         Ok(())
     }
 
     #[test]
     fn test_part_one_data() -> miette::Result<()> {
-        assert_eq!("2105", process(include_str!("day02_data.txt"))?);
+        assert_eq!(2105, process(include_str!("day02_data.txt"))?);
         Ok(())
     }
 }
