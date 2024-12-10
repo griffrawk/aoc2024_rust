216bed98e03abe0e3fdd763d7667974550a76fb2
Andy Griffiths <griffiths_andy@icloud.com>
2024-12-10 13:42:42 +0000

    1.2

--- src/day_01/day01.rs
+++ src/day_01/day01.rs
@@ -2,25 +2,43 @@ use std::fs;
 use std::iter::zip;
 use itertools::Itertools;
 
+struct Locations {
+    s1: Vec<i32>,
+    s2: Vec<i32>,
+}
+
+impl Locations {
+    fn new(file: &str) -> Self {
+        let contents: String = fs::read_to_string(file).expect("Can't read the file");
+        let mut s1: Vec<i32> = Vec::new();
+        let mut s2: Vec<i32> = Vec::new();
+        for line in contents.lines() {
+            let values: Vec<&str> = line.split_whitespace().collect();
+            s1.push(values[0].parse::<i32>().unwrap());
+            s2.push(values[1].parse::<i32>().unwrap());
+        }
+        Self {
+            s1,
+            s2,
+        }
+    }
+}
+
 #[allow(dead_code)]
 pub fn part_one(file: &str) -> i32 {
-    let contents: String = fs::read_to_string(file).expect("Can't read the file");
-    let mut s1: Vec<i32> = Vec::new();
-    let mut s2: Vec<i32> = Vec::new();
-    for line in contents.lines() {
-        let values: Vec<&str> = line.split_whitespace().collect();
-        s1.push(values[0].to_string().parse::<i32>().unwrap());
-        s2.push(values[1].to_string().parse::<i32>().unwrap());
-    }
-    let zipped = zip(s1.iter().sorted(), s2.iter().sorted()); 
+    let locations = Locations::new(file);
+    let zipped = zip(locations.s1.iter().sorted(), locations.s2.iter().sorted());
     zipped.fold(0, | acc, s| acc + (s.0 - s.1).abs())
 }
 
 #[allow(dead_code)]
-fn part_two(file: &str) -> u32 {
-    let contents = fs::read_to_string(file).expect("Can't read the file");
-
-    0
+fn part_two(file: &str) -> i32 {
+    let locations = Locations::new(file);
+    let mut tot = 0;
+    for a in locations.s1 {
+        tot += a * locations.s2.iter().filter(|&n| *n == a).count() as i32;
+    }
+    tot
 }
 
 #[cfg(test)]
@@ -41,13 +59,13 @@ mod tests {
 
     #[test]
     fn test_part_two() {
-        let result = part_two("src/day_01/day01_test_part2.txt");
-        // assert_eq!(result, 281);
+        let result = part_two("src/day_01/day01_test.txt");
+        assert_eq!(result, 31);
     }
 
     #[test]
     fn test_part_two_data() {
         let result = part_two("src/day_01/day01_data.txt");
-        // assert_eq!(result, 54100);
+        assert_eq!(result, 21306195);
     }
 }
