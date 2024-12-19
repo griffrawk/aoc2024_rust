c674b1d28c1b2eedcc7d4b2150d2cf498b6544a8
Andy Griffiths <griffiths_andy@icloud.com>
2024-12-19 17:02:24 +0000

    4.1

--- src/day_04/day04.rs
+++ src/day_04/day04.rs
@@ -1,25 +1,85 @@
-use std::cmp::{max, min};
-use std::collections::HashMap;
+use std::cmp::max;
 use std::fs;
-// use regex::Regex;
+
+/// Take Vec<String> and return a String consisting of contents read diagonally
+/// in NE direction (45 degrees clockwise from vertical if you like...)
+fn stringify_rot45(input: &Vec<String>) -> String {
+    let max_x = input[0].len();
+    let max_y = input.len();
+    let n = max(max_y, max_x);
+    let mut res = String::new();
+    // 1st half
+    for base_y in 0..n {
+        let mut y= base_y;
+        for x in 0..=base_y {
+            // need to check bounds and ignore
+            if x < max_x && y < max_y {
+                res.push_str(input[y].get(x..x+1).unwrap());
+            }
+            if y > 0 {y -= 1}
+        }
+        // delimit the line to avoid wraparound searching when as a flat String
+        res.push_str("|");
+    }
+    // 2nd half
+    for base_x in 1..n {
+        let mut x = base_x;
+        for y in (base_x..n).rev()  {
+            // need to check bounds and ignore
+            if x < max_x && y < max_y {
+                res.push_str(input[y].get(x..x+1).unwrap());
+            }
+            x += 1;
+        }
+        // delimit the line to avoid wraparound searching when as a flat String
+        res.push_str("|");
+    }
+    res
+}
+
+fn rot90(input: &Vec<String>) -> Vec<String> {
+    let m = input[0].len();
+    let n = input.len();
+    let mut res: Vec<String> = Vec::new();
+    for x in (0..m).rev() {
+        let mut line = String::new();
+        for y in 0..n {
+            line.push_str(input[y].get(x..x+1).unwrap());
+        }
+        // delimit the line to avoid wraparound searching when as a flat String
+        line.push_str("|");
+        res.push(line);
+    }
+    res
+}
 
 #[allow(dead_code)]
-pub fn part_one(file: &str) -> i32 {
+pub fn part_one(file: &str) -> usize {
     let mut res = 0;
     let contents = fs::read_to_string(file).expect("Can't read the file");
     let mut wordsearch: Vec<String> = Vec::new();
     for line in contents.lines() {
         wordsearch.push(line.to_string());
-
     }
-    dbg!(&wordsearch);
-
+    // delimit the rot0 line to avoid wraparound searching when as a flat String
+    // done beforehand in the rot45 & rot90 methods, so not needed again
+    let w0 = wordsearch.join("|");
+    let w45 = stringify_rot45(&wordsearch);
+    let w90 = rot90(&wordsearch).join("");
+    let w135 = stringify_rot45(&rot90(&wordsearch));
+    res += w0.matches("XMAS").collect::<Vec<_>>().len();
+    res += w0.matches("SAMX").collect::<Vec<_>>().len();
+    res += w45.matches("XMAS").collect::<Vec<_>>().len();
+    res += w45.matches("SAMX").collect::<Vec<_>>().len();
+    res += w90.matches("XMAS").collect::<Vec<_>>().len();
+    res += w90.matches("SAMX").collect::<Vec<_>>().len();
+    res += w135.matches("XMAS").collect::<Vec<_>>().len();
+    res += w135.matches("SAMX").collect::<Vec<_>>().len();
     res
 }
 
 #[allow(dead_code)]
 pub fn part_two(file: &str) -> i32 {
-
     0
 }
 
@@ -30,24 +90,24 @@ mod tests {
     #[test]
     fn test_part_one_test() {
         let result = part_one("src/day_04/day04_test.txt");
-        // assert_eq!(result, 18);
+        assert_eq!(result, 18);
     }
 
     #[test]
     fn test_part_one_data() {
         let result = part_one("src/day_04/day04_data.txt");
-        assert_eq!(result, 161289189);
+        assert_eq!(result, 2599);
     }
 
     #[test]
     fn test_part_two_test() {
         let result = part_two("src/day_04/day04_test.txt");
-        assert_eq!(result, 48);
+        assert_eq!(result, 0);
     }
 
     #[test]
     fn test_part_two_data() {
         let result = part_two("src/day_04/day04_data.txt");
-        assert_eq!(result, 83595109);
+        assert_eq!(result, 0);
     }
 }
--- src/day_04/rotate_45.rs
+++ src/day_04/rotate_45.rs
@@ -1,45 +1,83 @@
-// #include <bits/stdc++.h>
-// using namespace std;
+use std::cmp::max;
 
 // Function to rotate matrix by 45 degree
-fn rotate_45(li: Vec<Vec<usize>>) {
+// not very efficient though
+// for an 8*8 array it hits the push 1:14
+// for 9*9 it's 1:16
+// so the loss gets worse as size increases
+// for a big array in the data file, it will be huge
+//
+// another problem, if the array isn't square it panics with out-of-bounds errors
+#[allow(dead_code)]
+fn rotate_45_nope(li: Vec<Vec<usize>>) {
     let n = li.len();
     let m = li[0].len();
+    let mut hits = 0;
+    let mut misses = 0;
     let mut ctr: usize = 0;
     while ctr < 2 * n - 1 {
         let mut lst: Vec<usize> = Vec::new();
         for i in 0..m as usize{
             for j in 0..n as usize{
                 if i + j == ctr {
+                    hits += 1;
                     lst.push(li[i][j])
+                } else {
+                    misses += 1;
                 }
             }
         }
         println!("{:?}", lst);
         ctr += 1;
     }
+    println!("Hits: {}, Misses: {}", hits, misses);
+}
+
+// works with non-square arrays
+fn rotate_45_alt(li: Vec<Vec<usize>>) {
+    let max_x = li[0].len();
+    let max_y =li.len();
+    let n = max(max_y, max_x);
+    println!("First half\n");
+    for base_y in 0..n {
+        let mut y= base_y;
+        for x in 0..=base_y {
+            // need to check bounds and ignore
+            if x < max_x && y < max_y {
+                println!("x: {} y: {}, li {}", x, y, li[y][x]);
+            }
+            if y > 0 {y -= 1}
+        }
+        print!("\n");
+    }
+    println!("Second half\n");
+    for base_x in 1..n {
+        let mut x = base_x;
+        for y in (base_x..n).rev()  {
+            if x < max_x && y < max_y {
+                println!("x: {} y: {}, li {}", x, y, li[y][x]);
+            }
+            x += 1;
+        }
+        print!("\n");
+    }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day_04::rotate_45::rotate_45;
+    use crate::day_04::rotate_45::rotate_45_alt;
 
     #[test]
-    fn test_rotate_45() {
-        let n = 8;
-        let m = n;
+    fn test_rotate_45_alt() {
 
         let li = vec![
-            vec![4, 5, 6, 9, 8, 7, 1, 4],
-            vec![1, 5, 9, 7, 5, 3, 1, 6],
-            vec![7, 5, 3, 1, 5, 9, 8, 0],
-            vec![6, 5, 4, 7, 8, 9, 3, 7],
-            vec![3, 5, 6, 4, 8, 9, 2, 1],
-            vec![3, 1, 6, 4, 7, 9, 5, 0],
-            vec![8, 0, 7, 2, 3, 1, 0, 8],
-            vec![7, 5, 3, 1, 5, 9, 8, 5],
+            vec![3, 9, 4, 5, 6, 9, 8],
+            vec![3, 9, 4, 5, 6, 9, 8],
+            vec![3, 9, 4, 5, 6, 9, 8],
+            vec![3, 9, 4, 5, 6, 9, 8],
+            vec![2, 3, 6, 3, 8, 1, 4],
         ];
-        rotate_45(li);
+        rotate_45_alt(li);
 
     }
 }
