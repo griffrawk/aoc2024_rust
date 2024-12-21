03ac702bdcac9832204e02411ae7d499a7babfbd
Andy Griffiths <griffiths_andy@icloud.com>
2024-12-21 21:52:16 +0000

    6.2

--- src/day_06/day06.rs
+++ src/day_06/day06.rs
@@ -1,14 +1,15 @@
-use std::collections::HashMap;
+use std::cmp::PartialEq;
+use std::collections::{HashMap, HashSet};
 use std::fs;
 use std::ops::Range;
 
-#[derive(Debug, Eq, Hash, PartialEq)]
+#[derive(Debug, Clone, Eq, Hash, PartialEq)]
 struct Point {
     x: i32,
     y: i32,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
 enum Direction {
     North,
     East,
@@ -16,7 +17,7 @@ enum Direction {
     West,
 }
 
-#[derive(Debug)]
+#[derive(Debug, Clone)]
 struct Guard {
     pos: Point,
     xrange: Range<i32>,
@@ -80,7 +81,7 @@ impl Guard {
                     poss_direction = Direction::North;
                 }
             }
-            if !obstacles.obstacles.contains_key(&poss) {
+            if !obstacles.obstacles.contains(&poss) {
                 // valid move
                 self.pos = poss;
                 break;
@@ -91,38 +92,55 @@ impl Guard {
         }
     }
 
-    fn walk_guard(&mut self, obstacles: &Obstacles) -> HashMap<Point, usize> {
-        let mut visited: HashMap<Point, usize> = HashMap::new();
+    fn walk_guard(&mut self, obstacles: &Obstacles) -> (HashMap<Point, Direction>, bool) {
+        let mut visited: HashMap<Point, Direction> = HashMap::new();
+        let mut stuck = false;
         // while guard still on grid
         while self.xrange.contains(&self.pos.x) && self.yrange.contains(&self.pos.y) {
+            // check if guard has been here before in same direction, stuck if so...
+            let previous = visited.get(&Point {
+                x: self.pos.x,
+                y: self.pos.y,
+            });
+            match previous {
+                Some(s) => {
+                    if *s == self.direction {
+                        // been here before in same direction
+                        stuck = true;
+                        break;
+                    }
+                }
+                None => {}
+            }
             // record guard position as visited
             visited.insert(
                 Point {
                     x: self.pos.x,
                     y: self.pos.y,
                 },
-                0,
+                self.direction,
             );
             self.move_guard(&obstacles);
         }
-        visited
+        (visited, stuck)
     }
 }
 
+#[derive(Debug, Clone, Eq, PartialEq)]
 struct Obstacles {
-    obstacles: HashMap<Point, usize>,
+    obstacles: HashSet<Point>,
 }
 
 impl Obstacles {
     fn new(file: &str) -> Obstacles {
-        let mut obstacles: HashMap<Point, usize> = HashMap::new();
+        let mut obstacles: HashSet<Point> = HashSet::new();
         let mut y = 0;
         for line in fs::read_to_string(file)
             .expect("Can't read the file")
             .lines()
         {
             for (x, _) in line.match_indices("#") {
-                obstacles.insert(Point { x: x as i32, y: y }, 0);
+                obstacles.insert(Point { x: x as i32, y: y });
             }
             y += 1;
         }
@@ -136,12 +154,33 @@ pub fn part_one(file: &str) -> usize {
     let mut guard = Guard::new(&file);
 
     // return length of visited points
-    guard.walk_guard(&obstacles).len()
+    guard.walk_guard(&obstacles).0.len()
+}
+
+pub fn part_two(file: &str) -> usize {
+    let obstacles = Obstacles::new(&file);
+    let guard = Guard::new(&file);
+    let mut res = 0;
+
+    // get initial walk
+    let mut clone_guard = guard.clone();
+    let complete_walk = clone_guard.walk_guard(&obstacles);
+
+    // check if a new obs at any of the initial visited points would cause a loop
+    for new_obs in complete_walk.0 {
+        let mut clone_obstacles = obstacles.clone();
+        clone_obstacles.obstacles.insert(new_obs.0);
+        let mut clone_guard = guard.clone();
+        if clone_guard.walk_guard(&clone_obstacles).1 == true {
+            res += 1;
+        }
+    }
+    res
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day_06::day06::part_one;
+    use crate::day_06::day06::{part_one, part_two};
 
     #[test]
     fn test_part_one_test() {
@@ -153,5 +192,17 @@ mod tests {
     fn test_part_one_data() {
         let result = part_one("src/day_06/day06_data.txt");
         assert_eq!(result, 5095);
+    }
+
+   #[test]
+    fn test_part_two_test() {
+        let result = part_two("src/day_06/day06_test.txt");
+        assert_eq!(result, 6);
+    }
+
+    #[test]
+    fn test_part_two_data() {
+        let result = part_two("src/day_06/day06_data.txt");
+        assert_eq!(result, 1933);
     }
 }
