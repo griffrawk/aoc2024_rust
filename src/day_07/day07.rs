cf979360932bf193f8f91dc6b8b98c2f2f796b84
Andy Griffiths <griffiths_andy@icloud.com>
2024-12-23 12:25:45 +0000

    7.1

--- src/day_07/day07.rs
+++ src/day_07/day07.rs
@@ -1,61 +1,76 @@
-use std::collections::{HashMap, HashSet};
 use std::fs;
-use std::ops::Range;
 
-fn bin_count(bits: usize) -> Vec<String> {
+#[derive(Debug)]
+enum Operator {
+    Add,
+    Mul,
+}
+fn operations(bits: usize) -> Vec<Vec<Operator>> {
+    let mut opsteps: Vec<Vec<Operator>> = Vec::new();
     let z = (bits as f32).exp2() as usize;
-    (0..z).into_iter().map(|n| {
-        format!("{:0b$b}", n, b = bits)
-    }).collect()
+    for n in 0..z {
+        let y = format!("{:0b$b}", n, b = bits);
+        let mut opseq: Vec<Operator> = Vec::new();
+        for s in y.chars() {
+            match s {
+                '0' => { opseq.push(Operator::Add) },
+                '1' => { opseq.push(Operator::Mul) },
+                _ => panic!()
+            }
+        }
+        opsteps.push(opseq);
+    }
+    opsteps
 }
 
 #[allow(dead_code)]
-pub fn part_one(file: &str) -> usize {
+pub fn part_one(file: &str) -> i64 {
+    let mut res = 0;
     let contents: String = fs::read_to_string(file).expect("Can't read the file");
-
-
-    0
+    for line in contents.lines() {
+        let eqn: Vec<i64> = line
+            .split_whitespace()
+            .filter_map(|s| s.replace(":", "").parse::<i64>().ok())
+            .collect();
+        // result is eqn[0], others are operands, so we need as many opsteps
+        // as there are gaps between operands
+        let opsteps = operations(eqn.len() - 2);
+        for opseq in opsteps {
+            let mut sum: i64 = eqn[1];
+            for (idx, op) in opseq.into_iter().enumerate() {
+                match op {
+                    Operator::Add => sum += eqn[idx + 2],
+                    Operator::Mul => sum *= eqn[idx + 2],
+                }
+            }
+            if sum == eqn[0] {
+                res += sum;
+                break;
+            }
+        }
+    }
+    res
 }
 
-pub fn part_two(file: &str) -> usize {
+pub fn part_two(file: &str) -> i32 {
 
     0
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day_07::day07::{bin_count, part_one };
-
-    #[test]
-    fn test_bin_count() {
-        assert_eq!(bin_count(3), vec![
-            "000".to_string(),
-            "001".to_string(),
-            "010".to_string(),
-            "011".to_string(),
-            "100".to_string(),
-            "101".to_string(),
-            "110".to_string(),
-            "111".to_string(),
-        ]);
-        assert_eq!(bin_count(2), vec![
-            "00".to_string(),
-            "01".to_string(),
-            "10".to_string(),
-            "11".to_string(),
-        ])
-    }
+    use crate::day_07::day07::{part_one };
 
     #[test]
     fn test_part_one_test() {
         let result = part_one("src/day_07/day07_test.txt");
-        assert_eq!(result, 41);
+        assert_eq!(result, 3749);
     }
 
     #[test]
     fn test_part_one_data() {
         let result = part_one("src/day_07/day07_data.txt");
-        assert_eq!(result, 5095);
+        assert_eq!(result, 1430271835320);
     }
 
 }
