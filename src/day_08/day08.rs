e7411a34d0dac75fde5e5e0ae5503bd512c0e21c
Andy Griffiths <griffiths_andy@icloud.com>
2025-01-12 23:01:07 +0000

    8.1 complete

--- src/day_08/day08.rs
+++ src/day_08/day08.rs
@@ -1,36 +1,46 @@
 use aocutils::point::Point;
 use std::collections::{HashMap, HashSet};
 use std::fs;
+use std::iter::zip;
 use std::ops::Range;
+use itertools::Itertools;
 
 #[derive(Debug, Clone, Eq, PartialEq)]
-struct Antennae {
+struct City {
     antennae: HashMap<char, Vec<Point<i32>>>,
     xrange: Range<i32>,
     yrange: Range<i32>,
 }
 
-impl Antennae {
-    fn new(file: &str) -> Antennae {
+impl City {
+    fn new(file: &str) -> City {
         let mut antennae: HashMap<char, Vec<Point<i32>>> = HashMap::new();
         let mut max_x = 0;
         let mut max_y = 0;
         for (y, line) in fs::read_to_string(file)
             .expect("Can't read the file")
-            .lines().enumerate()
+            .lines()
+            .enumerate()
         {
             max_x = line.len() as i32;
             max_y += 1;
-            // only A for the mo, need to expand this to catch 0 to 9, A to Z, a to z
             for (x, g) in line.match_indices(|g: char| g.is_ascii_alphanumeric()) {
                 let group = g.chars().next().expect("String is empty");
-                antennae.entry(group)
-                    .and_modify(|e| e.push(Point{x: x as i32, y: y as i32}))
-                    .or_insert(vec!(Point{ x: x as i32, y: y as i32}));
-
+                antennae
+                    .entry(group)
+                    .and_modify(|e| {
+                        e.push(Point {
+                            x: x as i32,
+                            y: y as i32,
+                        })
+                    })
+                    .or_insert(vec![Point {
+                        x: x as i32,
+                        y: y as i32,
+                    }]);
             }
         }
-        Antennae {
+        City {
             antennae,
             xrange: 0..max_x,
             yrange: 0..max_y,
@@ -38,17 +48,44 @@ impl Antennae {
     }
 }
 
+fn calc_antinodes(a: &Point<i32>, b: &Point<i32>) -> Vec<Point<i32>> {
+    let dx = a.x - b.x;
+    let dy = a.y - b.y;
+    vec!(Point { x: a.x + dx, y: a.y + dy }, Point { x: b.x - dx, y: b.y - dy })
+}
+
 #[allow(dead_code)]
 pub fn part_one(file: &str) -> usize {
-    let antennae = Antennae::new(&file);
-    dbg!(antennae);
+    let city = City::new(&file);
+    let mut antinodes: HashSet<Point<i32>> = HashSet::new();
 
-    14
+    for (_, group) in city.antennae {
+        for ainc in 0..group.len() - 1 {
+            let a = &group[ainc];
+            for binc in ainc+1..group.len() {
+                let b = &group[binc];
+                for antinode in calc_antinodes(a, b) {
+                    if city.xrange.contains(&antinode.x) && city.yrange.contains(&antinode.y) {
+                        antinodes.insert(antinode);
+                    }
+                }
+            }
+        }
+    }
+    antinodes.len()
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day_08::day08::{part_one,};
+    use crate::day_08::day08::{part_one, calc_antinodes};
+    use aocutils::point::Point;
+
+    #[test]
+    fn test_antinodes() {
+        let a: Point<i32> = Point { x: 8, y: 1};
+        let b: Point<i32> = Point { x: 5, y: 2};
+        dbg!(&calc_antinodes(&a, &b));
+    }
 
     #[test]
     fn test_part_one_test() {
@@ -58,8 +95,7 @@ mod tests {
 
     #[test]
     fn test_part_one_data() {
-        let result = part_one("src/day_06/day06_data.txt");
-        assert_eq!(result, 5095);
+        let result = part_one("src/day_08/day08_data.txt");
+        assert_eq!(result, 369);
     }
-
 }
