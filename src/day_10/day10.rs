e912f62baca95974cc55c530d287464e92b48410
Andy Griffiths <griffiths_andy@icloud.com>
2025-01-17 13:45:40 +0000

    10.1 in progress

--- src/day_10/day10.rs
+++ src/day_10/day10.rs
@@ -1,30 +1,89 @@
+use aocutils::point::Point;
+use std::collections::{HashMap, HashSet};
 use std::fs;
+use std::ops::Range;
 
 // Part 1 structs & impl
 #[derive(Debug)]
-struct Disk {
-    // blocks: Vec<Option<usize>>,
+struct TopoMap {
+    heights: HashMap<Point<i32>, u32>,
+    trailheads: Vec<Point<i32>>,
+    xrange: Range<i32>,
+    yrange: Range<i32>,
+    res: usize,
 }
 
-impl Disk {
-    fn new(file: &str) -> Disk {
-        // let mut blocks = Vec::new();
-        for (map_count, map_block) in fs::read_to_string(file)
+impl TopoMap {
+    fn new(file: &str) -> TopoMap {
+        let mut heights = HashMap::new();
+        let mut trailheads = Vec::new();
+        let mut xrange = 0..1;
+        let mut yrange = 0..1;
+        for (y, line) in fs::read_to_string(file)
             .expect("Can't read the file")
-            .chars()
+            .lines()
             .enumerate()
         {
-            // do something
+            xrange = 0..line.len() as i32;
+            yrange = 0..y as i32 + 1;
+            for (x, height) in line.chars().enumerate() {
+                let height = height.to_digit(10).unwrap_or_default();
+                heights.insert(
+                    Point {
+                        x: x as i32,
+                        y: y as i32,
+                    },
+                    height,
+                );
+                if height == 0 {
+                    trailheads.push(Point {x: x as i32, y: y as i32});
+                }
+            }
         }
-        Disk { }
+        let res = 0;
+        TopoMap {
+            heights,
+            trailheads,
+            xrange,
+            yrange,
+            res,
+        }
+    }
+
+    fn walk_trails(&mut self) -> usize {
+        for head in self.trailheads.clone() {
+            self.walk(head);
+        }
+        self.res
+    }
+
+    fn walk(&mut self, pos: Point<i32>) {
+        let mut visited: HashSet<&Point<i32>> = HashSet::new();
+        let height = self.heights[&pos];
+        if visited.contains(&pos) { return }
+        if height == 9 {
+            self.res += 1;
+            return;
+        }
+        visited.insert(&pos);
+        // look for next higher
+        let next_height = height + 1;
+        // check N, E, S, W
+        for (dx, dy)  in [(0, -1), (1, 0), (0, 1), (-1, 0)] {
+            let next_pos = Point { x: pos.x + dx, y: pos.y + dy };
+            if self.xrange.contains(&next_pos.x) && self.yrange.contains(&next_pos.y) {
+                if self.heights[&next_pos] == next_height {
+                    self.walk(next_pos)
+                }
+            }
+        }
     }
 }
 
 // mains
 #[allow(dead_code)]
 pub fn part_one(file: &str) -> usize {
-
-    999
+    TopoMap::new(&file).walk_trails()
 }
 
 #[allow(dead_code)]
@@ -39,7 +98,7 @@ mod tests {
     #[test]
     fn test_part_one_test() {
         let result = part_one("src/day_10/day10_test.txt");
-        assert_eq!(result, 999);
+        assert_eq!(result, 36);
     }
 
     #[test]
