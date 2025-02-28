ba9541ae4377d7fc4dd20266fa89779e90255909
Andy Griffiths <griffiths_andy@icloud.com>
2025-02-28 01:58:45 +0000

    16.1 with A*

--- src/bin/day16/main.rs
+++ src/bin/day16/main.rs
@@ -1,15 +1,15 @@
-mod day16_part_one;
+mod day16_graph;
 
 fn main() {
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::day16_part_one::Graph;
+    use crate::day16_graph::Graph;
 
     #[test]
     fn test_part_one_test_a() {
-        let mut graph = Graph::new("src/bin/day16/day16_test_a.txt");
+        let mut graph = Graph::new("src/bin/day16/data/day16_test_a.txt");
         if let Some(res) = graph.shortest_path() {
             graph.visual_plot(true).unwrap();
             assert_eq!(res, 7036);
@@ -18,7 +18,7 @@ mod tests {
 
     #[test]
     fn test_part_one_test_b() {
-        let mut graph = Graph::new("src/bin/day16/day16_test_b.txt");
+        let mut graph = Graph::new("src/bin/day16/data/day16_test_b.txt");
         if let Some(res) = graph.shortest_path() {
             graph.visual_plot(true).unwrap();
             assert_eq!(res, 11048);
@@ -27,7 +27,7 @@ mod tests {
 
     #[test]
     fn test_part_one_data() {
-        let mut graph = Graph::new("src/bin/day16/day16_data.txt");
+        let mut graph = Graph::new("src/bin/day16/data/day16_data.txt");
         if let Some(res) = graph.shortest_path() {
             graph.visual_plot(true).unwrap();
             assert_eq!(res, 107512);
@@ -37,7 +37,7 @@ mod tests {
     #[test]
     fn test_part_one_astar() {
         // let mut graph = Graph::new("src/bin/day16/day16_test_a.txt");
-        let mut graph = Graph::new("src/bin/day16/day16_data.txt");
+        let mut graph = Graph::new("src/bin/day16/data/day16_data.txt");
         if let Some(res) = graph.astar() {
             graph.astar_visual_plot(true).unwrap();
             dbg!(res);
