aa5c692fdc89fed15a946c1adb19339063a209e5
Andy Griffiths <griffiths_andy@icloud.com>
2025-03-25 15:15:58 +0000

    17 change to 64-bit registers

--- src/bin/day17/cpu.rs
+++ src/bin/day17/cpu.rs
@@ -1,12 +1,11 @@
 use std::fs;
-use num::ToPrimitive;
 
 #[derive(Debug)]
 pub struct CPU {
     instruction_pointer: usize,
-    reg_a: usize,
-    reg_b: usize,
-    reg_c: usize,
+    reg_a: u64,
+    reg_b: u64,
+    reg_c: u64,
 
     pub(crate) program: Vec<usize>,
     pub(crate) output: Vec<usize>,
@@ -70,14 +69,14 @@ impl CPU {
     // opcode 0 - divide A
     fn adv(&mut self, combo: usize) {
         if let Some(o) = self.combo_value(combo) {
-            self.reg_a = (self.reg_a as i32 / 2i32.pow(o as u32)).to_usize().unwrap();
+            self.reg_a = self.reg_a / 2u64.pow(o as u32);
         }
         self.instruction_pointer += 2;
     }
 
     // opcode 1 - B XOR literal
     fn bxl(&mut self, literal: usize) { 
-        self.reg_b = self.reg_b ^ literal;
+        self.reg_b = self.reg_b ^ literal as u64;
         self.instruction_pointer += 2;
     }
 
@@ -104,28 +103,30 @@ impl CPU {
 
     fn out(&mut self, combo: usize) {
         if let Some(o) = self.combo_value(combo) {
-            self.output.push(o.rem_euclid(8));
+            self.output.push(o.rem_euclid(8) as usize);
         }
         self.instruction_pointer += 2;
     }
 
     fn bdv(&mut self, combo: usize) {
         if let Some(o) = self.combo_value(combo) {
-            self.reg_b = (self.reg_a as i32 / 2i32.pow(o as u32)).to_usize().unwrap();
+            self.reg_b = self.reg_a / 2u64.pow(o as u32);
+            
         }
         self.instruction_pointer += 2;
     }
 
     fn cdv(&mut self, combo: usize) {
         if let Some(o) = self.combo_value(combo) {
-            self.reg_c = (self.reg_a as i32 / 2i32.pow(o as u32)).to_usize().unwrap();
+            self.reg_c = self.reg_a / 2u64.pow(o as u32);
+            
         }
         self.instruction_pointer += 2;
     }
     
-    fn combo_value(&mut self, combo: usize) -> Option<usize> {
+    fn combo_value(&mut self, combo: usize) -> Option<u64> {
         match combo {
-            0..=3 => Some(combo),
+            0..=3 => Some(combo as u64),
             4 => Some(self.reg_a),
             5 => Some(self.reg_b),
             6 => Some(self.reg_c),
--- src/bin/day17/main.rs
+++ src/bin/day17/main.rs
@@ -9,7 +9,7 @@ mod tests {
 
     #[test]
     fn test_part_one_test() {
-        let mut cpu = CPU::new("src/bin/day17/data/day17_test_part2.txt");
+        let mut cpu = CPU::new("src/bin/day17/data/day17_test.txt");
         assert_eq!(cpu.run(), "4,6,3,5,6,3,5,2,1,0");
     }
 
